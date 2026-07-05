09e915d0c78e3b1c6b89a5294c61e1c598ad9c52
oat431 <oat431@gmail.com>
2026-07-05 19:33:54 +0700

    chore: add tailwind to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3300,
     proxy: {
-      '/api': 'http://localhost:8005',
+      "/api": "http://localhost:8005",
     },
   },
 });
