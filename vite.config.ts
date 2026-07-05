6f3ce1ed7282c1b6d4fd5eb01ceb82f05cfa4f7c
oat431 <oat431@gmail.com>
2026-07-05 18:56:57 +0700

    chore: change default port

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3300,
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://localhost:8005',
     },
   },
-})
+});
